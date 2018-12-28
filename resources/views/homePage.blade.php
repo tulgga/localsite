d90a013b7ad108c5670ecd6d8f9dea872a6d063d
Ankhbaatar <bolloomn@gmail.com>
2018-12-28 09:28:24 +0800

    updated

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -40,8 +40,8 @@
             </ul>
             <ul class="col-sm-6 right-menu">
                 <li><a href="#" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-list-ul"></i> Сумдын веб хуудас</a></li>
-                <li><a href="#"><i class="fa fa-sitemap"></i> Агентлагуудын холбоос</a></li>
-                <li><a href="#"><i class="fa fa-lightbulb-o"></i> Санал хүсэлт, өргөдөл гомдол</a></li>
+                <li><a href="#" data-toggle="modal" data-target=".bd-example-modal-lg-2"><i class="fa fa-sitemap"></i> Агентлагуудын холбоос</a></li>
+                <li><a href="http://bayankhongor.towersoft.mn/!#/report"><i class="fa fa-lightbulb-o"></i> Санал хүсэлт, өргөдөл гомдол</a></li>
             </ul>
         </div>
     </div>
@@ -59,8 +59,8 @@
                 <ul class="row">
                     @foreach($sumuud as $domain)
                         @if(!$domain->id == 0)
-                            <li class="col-sm-6">
-                                <a href="{{$domain->domain}}"><img src="{{ asset('uploads/'.$domain->favicon)}}"> {{$domain->name}}</a>
+                            <li class="col-6">
+                                <a href="{{$domain->domain}}">@if($domain->favicon)<img src="{{ asset('uploads/'.$domain->favicon)}}">@else<img src="{{ asset('uploads/favicon/XnjMhMUg3enV21GhLYDopC2L6uHsuQK0BqjTjCjP.png')}}">@endif {{$domain->name}}</a>
                             </li>
                         @endif
                     @endforeach
@@ -103,8 +103,9 @@
                                             </h2>
                                         </div>
                                         <div id="item1_{{$i}}" class="collapse" aria-labelledby="item_{{$i}}" data-parent="#accordionExample1">
-                                            @if(isset($item['item']))
                                             <div class="card-body">
+                                            @if(isset($item['item']))
+                                            <div class="accordion" id="accordionExample2">
                                                 @foreach($item['item'] as $i=>$itm)
                                                     <div class="card">
                                                         <div class="card-header" id="itm_{{$i}}">
@@ -117,18 +118,21 @@
                                                         <div id="itm1_{{$i}}" class="collapse" aria-labelledby="itm_{{$i}}" data-parent="#accordionExample2">
                                                             <div class="card-body">
                                                                 <ol style="list-style: inside decimal-leading-zero;">
-
+                                                                    <li>Хэрэгтэй мэдээлэл болон бүрдүүлэх материалын жагсаалт орно...</li>
                                                                 </ol>
                                                             </div>
                                                         </div>
                                                     </div>
+                                                    
                                                 @endforeach
-                                            </div>
+                                                </div>
+                                            
                                             @else
                                                 <ol style="list-style: inside decimal-leading-zero;">
-                                                   
+                                                   <li>Хэрэгтэй мэдээлэл болон бүрдүүлэх материалын жагсаалт орно...</li>
                                                 </ol>
                                             @endif
+                                            </div>
                                         </div>
                                     </div>
                                     @endforeach
@@ -138,6 +142,52 @@
                     </div>
                     @endforeach
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade bd-example-modal-lg-2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content sumd">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Аймгийн хэлтэс агентлагууд</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <ul class="row">
+                        <li class="col-sm-6"> <a target="_blank" href="http://www.audit.mn/?page_id=1314">Аймгийн аудитийн газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Байгаль орчин, аялал жуулчлалын газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://www.bayankhongor.moh.gov.mn/emg/">Эрүүл мэндийн газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bayankhongor.gov.mn/top/31">Орон нутгийн өмчийн газар </a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Хөгжимт драмын театр</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://www.ndaatgal.bkh.gov.mn">Нийгмийн даатгалын хэлтэс</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Стандарт, хэмжил зүйн хэлтэс</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bkh.burtgel.gov.mn/">Улсын бүртгэлийн хэлтэс </a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Тагнуулын хэлтэс</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Шүүхийн шинжилгээний алба</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://www.bayankhongor.nema.gov.mn/">Онцгой байдлын газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Гэр бүл,хүүхэд, залуучуудын хөгжлийн газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bayankhongor.gazar.gov.mn/">ГХБХБГазар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Хүүхдийн Тэмүүжин театр</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Хүнс, хөдөө аж ахуйн газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://www.bayankhongor.court.gov.mn/">Аймгийн шүүх</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Түүх угсаатны зүйн музей</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://www.bayankhongor.nso.mn/index.php">Статистикийн хэлтэс </a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bayankhongor.gov.mn/116/single">Санхүүгийн хяналт, аудитын алба </a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Биеийн тамир, спортын газар </a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Гаалийн алба </a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="www">Номын сан</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bkh.inspection.gov.mn/">Мэргэжлийн хяналтын газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bayankhongor.halamj.gov.mn">Хөдөлмөр, халамжийн үйлчилгээний газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://www.bayankhongor.police.gov.mn">Цагдаагийн газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bayankhongor.cd.gov.mn/">Шүүхийн шийдвэр гүйцэтгэх алба</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bayankhongor-bolovsrol.mn">Боловсрол, соёл, урлагийн газар</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://mta-bayankhongor.mn/">Татварын хэлтэс</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://prokuror.bkh.gov.mn/ ">Аймгийн прокурор</a> </li>
+                        <li class="col-sm-6"> <a target="_blank" href="http://bayankhongor.tsag-agaar.gov.mn/">Ус, цаг уур, орчны шинжилгээний газар</a> </li>
+                      </ul>
             </div>
         </div>
     </div>
