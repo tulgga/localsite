9cb78ae52bc90ad45f9e6eedd4299cba2264842c
anhaa0702 <anhaa0702@gmail.com>
2019-05-08 22:32:53 +0800

    event going done api

--- app/Http/Controllers/Api/ApiEventController.php
+++ app/Http/Controllers/Api/ApiEventController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 use App\Dashboard_schedule;
+use App\Dashboard_schedule_going;
 use App\Http\Controllers\Controller;
 
+use App\User;
 use Illuminate\Support\Facades\DB;
 class ApiEventController extends Controller
 {
@@ -29,26 +31,18 @@ class ApiEventController extends Controller
         return response()->json( $events);
     }
 
-    public function going($id, $user_id=0,$ip="", $device="")
+    public function going($id, $user_id=0, $ip='', $device="")
     {
-//        $data=$this->MainData();
-//        $events = []; // event
-//
-//        if($site_id > 0){
-//            $events = DB::select("select id, schedule_date as date,head_id as org_type, start_time as start, end_time as end, description,person_count, ifnull(cnt, 0) as person_going_count from dashboard_schedules
-//                left join (
-//                    select dashboard_schedule_id, count(0) as cnt
-//                    from  Dashboard_schedule_going
-//                    where created_at >= '".$data['y']."'
-//                    group by dashboard_schedule_id
-//                ) going
-//
-//                on dashboard_schedules.id = going.dashboard_schedule_id
-//
-//                where is_publish =1 and schedule_date >= '".$data['y']."'" );
-//        }
-//
-//        return response()->json( $events);
+        if(count(Dashboard_schedule_going::where('dashboard_schedule_id',$id)->where('user_id',$user_id)->get())==0 ){
+            $go = new Dashboard_schedule_going();
+            $go->dashboard_schedule_id = $id;
+            $go->user_id = $user_id;
+            $go->ip = $ip;
+            $go->device = $device;
+            $go->save();
+            return response()->json(["status"=> 1, 'msg'=>"Амжилттай"]);
+        }
+        return response()->json(["status"=> 0, 'msg'=>"Энэ үйлдлийг хийх боломжгүй байна"]);
     }
 
 
