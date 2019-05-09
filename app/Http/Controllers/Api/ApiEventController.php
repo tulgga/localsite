59833a388f0ee66acc686884baf77e16ac3b3fd0
anhaa0702 <32154360+anhaa0702@users.noreply.github.com>
2019-05-09 16:54:32 +0800

    Update ApiEventController.php

--- app/Http/Controllers/Api/ApiEventController.php
+++ app/Http/Controllers/Api/ApiEventController.php
@@ -24,17 +24,19 @@ class ApiEventController extends Controller
                 WHEN head_id = 8 THEN 'Сумын ЗДТГын зааланд'
                 ELSE 'Бусад'
             END as org, start_time as start, 
+            ifnull(users, '') as users,
             end_time as end, description,person_count, ifnull(cnt, 0) as person_going_count 
             
             from dashboard_schedules 
                 left join (
-                    select dashboard_schedule_id, count(0) as cnt 
+                    select dashboard_schedule_id, count(0) as cnt, GROUP_CONCAT(user_id) as users 
                     from  dashboard_schedule_goings 
                     where created_at >= '".$data['y']."'
                     group by dashboard_schedule_id 
                 ) going on dashboard_schedules.id = going.dashboard_schedule_id
                 
-            where is_publish =1 and schedule_date >= '".$data['y']."'" );
+                
+            where head_id>3 and is_publish =1 and schedule_date >= '".$data['y']."'" );
         }
 
         return response()->json( ['success'=>$events]);
@@ -42,7 +44,7 @@ class ApiEventController extends Controller
 
     public function going($id, $user_id=0, $ip='', $device="")
     {
-        if(count(Dashboard_schedule_going::where('dashboard_schedule_id',$id)->where('user_id',$user_id)->get())==0 ){
+        if(count(Dashboard_schedule::where('id',$id)->where('head_id','>',3)->get())> 0 && count(Dashboard_schedule_going::where('dashboard_schedule_id',$id)->where('user_id',$user_id)->get())==0 ){
             $go = new Dashboard_schedule_going();
             $go->dashboard_schedule_id = $id;
             $go->user_id = $user_id;
