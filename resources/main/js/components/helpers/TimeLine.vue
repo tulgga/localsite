d25125ff209fb139a68c2387bd4e2ced78014b1e
Анхбаатар <ankhbaatar@egulen.com>
2019-05-10 00:35:35 +0800

    s

---
+++ resources/main/js/components/helpers/TimeLine.vue
@@ -0,0 +1,78 @@
+<template>
+    <div  v-if="post" >
+        <div class="timeline">
+            <header class="timeline-header">
+                <span class="tag is-medium is-success">Эхлэл</span>
+            </header>
+            <template v-for="p in post">
+                <div class="timeline-item">
+                    <div class="timeline-marker"></div>
+                    <div class="timeline-content">
+                        <div class="p-2" style="border:1px solid #dbdbdb">
+                            <p class="heading is-size-4" ><a @click="Modal=p ">{{p.title}}</a></p>
+                            <p>
+                                <img v-if="p.type===2" :src="'https://img.youtube.com/vi/'+p.image+'/0.jpg'"/>
+                                <img v-else :src="siteUrl+p.image.replace('images/', '/uploads/medium/')"/>
+                            </p>
+                            <p>{{p.short_content}}</p>
+
+                        </div>
+
+                    </div>
+                </div>
+            </template>
+
+
+            <div class="timeline-header">
+                <span class="tag is-medium is-success">Төгсгөл</span>
+            </div>
+        </div>
+
+        <div class="modal is-active" v-if="Modal">
+            <div class="modal-background" v-on:click="Modal = false"></div>
+
+            <div class="modal-card" style="max-width: 610px">
+                <div class="has-text-white has-background-primary" style="padding:20px;">{{Modal.title}}</div>
+                <section class="modal-card-body pd0">
+                    <p>
+                        <img v-if="Modal.type===2" :src="'https://img.youtube.com/vi/'+p.image+'/0.jpg'"/>
+                        <img v-else :src="siteUrl+Modal.image.replace('images/', '/uploads/medium/')"/>
+                    </p>
+                    <p>{{Modal.short_content}}</p>
+                </section>
+            </div>
+            <button v-on:click="Modal = false" class="modal-close is-large" aria-label="close"></button>
+        </div>
+    </div>
+    <loading v-else></loading>
+
+</template>
+
+<script>
+    export default {
+        props:[
+            'cat_id',
+        ],
+        data(){
+            return {
+                Modal:false,
+                page: false,
+                siteUrl: window.surl,
+                subdomain: window.subdomain,
+                post: [],
+            }
+        },
+        created: function () {
+            this.fetchData();
+        },
+
+        methods: {
+            fetchData: function () {
+                axios.get('CatTimeLine/'+this.cat_id).then((response) => {
+                    this.post=response.data.success;
+                    this.page=true;
+                })
+            },
+        }
+    }
+</script>
